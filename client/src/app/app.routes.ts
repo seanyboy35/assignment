f67197798e2d8b63efe02c4efcf0c1187ddcb6ce
sean de silva <seandsokc35@gmail.com>
2024-08-22 23:37:53 +1000

    app routes update commit

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './video-chat/video-chat.component';
-import { DashboardComponent } from './user-managment/user-managment.component';
+import { VideoChatComponent } from './video-chat/video-chat.component';
+import { UserManagementComponent } from './user-managment/user-managment.component';
 import { ChatComponent } from './chat/chat.component';
-import { AdminComponent } from './group-management/group-management.component';
+import { GroupManagementComponent } from './group-management/group-management.component';
 
 const routes: Routes = [
-  { path: '', component: LoginComponent },
-  { path: 'dashboard', component: DashboardComponent },
+  { path: '', component: VideoChatComponent },
+  { path: 'dashboard', component: UserManagementComponent },
   { path: 'chat', component: ChatComponent },
-  { path: 'admin', component: AdminComponent },
+  { path: 'admin', component: GroupManagementComponent },
 ];
 
 @NgModule({
