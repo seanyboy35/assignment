8c14f8c912b73cedadc39b449d73fcfc9cf73041
sean de silva <seandsokc35@gmail.com>
2024-10-10 00:15:55 +1000

    video chat ports commit

--- video-chat-server/server.js
+++ video-chat-server/server.js
@@ -2,8 +2,8 @@ const express = require('express');
 const { ExpressPeerServer } = require('peer');
 
 const app = express();
-const server = app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+const server = app.listen(3002, () => {
+  console.log('Server is running on port 3002');
 });
 
 const peerServer = ExpressPeerServer(server, {
